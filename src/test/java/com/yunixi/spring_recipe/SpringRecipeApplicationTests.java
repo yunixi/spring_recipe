38b8aebe9e18531fd5756cc99cce823b08b4961e
yunixi <jyjy2197@gmail.com>
2022-08-01 22:37:58 +0900

    Initial Commit

---
+++ src/test/java/com/yunixi/spring_recipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunixi.spring_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
